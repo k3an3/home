48f8d8b136d4960339058179ddd42bf47e2afbd8
Keane O'Kelley <keane.m.okelley@gmail.com>
2016-05-30 19:08:28 -0500

    Add checksum

--- bulb.py
+++ bulb.py
@@ -8,13 +8,13 @@ def change_color(red, green, blue, brightness=100):
     blue *= int(brightness / 100)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('172.24.1.248', 5577))
-    for i in range(0, 255):
-        data = bytearray.fromhex('31' + format(red, 'x').zfill(2)
-                                + format(green, 'x').zfill(2)
-                                + format(blue, 'x').zfill(2)
-                                + '00f00f' + format(i, 'x').zfill(2))
-        s.send(data)
+    s.connect(('10.10.123.3', 5577))
+    data = bytearray.fromhex('31' + format(red, 'x').zfill(2)
+                            + format(green, 'x').zfill(2)
+                            + format(blue, 'x').zfill(2)
+                            + '00f00f')
+    data.append(sum(data) % 256)
+    s.send(data)
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
