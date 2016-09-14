f779bdd76fafe89a57d2623bf1db1399aa6797c8
Keane O'Kelley <keane.m.okelley@gmail.com>
2016-09-14 15:39:31 -0500

    Update new layout

--- run.py
+++ run.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-from web.main import socketio
+import os
+import sys
 
-socketio.run()
+from web.main import socketio, app
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+try:
+    import eventlet
+    eventlet.monkey_patch()
+    print('Using eventlet')
+    create_thread_func = lambda f: f
+    start_thread_func = lambda f: eventlet.spawn(f)
+except ImportError:
+    try:
+        import gevent
+        import gevent.monkey
+        gevent.monkey.patch_all()
+        print('Using gevent')
+        create_thread_func = lambda f: gevent.Greenlet(f)
+        start_thread_func = lambda t: t.start()
+    except ImportError:
+        import threading
+        print('Using threading')
+        create_thread_func = lambda f: threading.Thread(target=f)
+        start_thread_func = lambda t: t.start()
+socketio.run(app, debug=True)
