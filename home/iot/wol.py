42bc80f33f27d0bfdd60f88a0c98810ff8c4f183
Keane O'Kelley <keane.m.okelley@gmail.com>
2017-02-10 22:00:54 -0600

    Fix naivity

--- home/iot/wol.py
+++ home/iot/wol.py
@@ -4,14 +4,20 @@ wol.py
 
 Wrapper for wake-on-lan automation
 """
+import subprocess
+
 from wakeonlan import wol
 
 
 class WakeOnLAN:
-    def __init__(self, mac, host=None, port=None):
+    def __init__(self, mac, host=None, port=None, manual_interface=None):
         self.mac = mac
         self.host = host
         self.port = port
+        self.interface = manual_interface
 
     def wake(self):
-        wol.send_magic_packet(self.mac, ip_address=self.host, port=self.port)
+        if self.interface:
+            subprocess.run(['sudo', '/usr/sbin/ether-wake', '-i', self.interface, self.mac])
+        else:
+            wol.send_magic_packet(self.mac, ip_address=self.host, port=self.port)
