b4b0c57080de95aa6459c6e03487a7884934ef6b
Keane O'Kelley <kokelley@iastate.edu>
2017-03-21 22:38:50 -0500

    Make ping more flexible

--- home/iot/ping.py
+++ home/iot/ping.py
@@ -4,16 +4,18 @@ from home.web.utils import send_to_subscribers
 
 
 class Ping:
-    def __init__(self, host, port):
+    def __init__(self, host=None, port=None):
         self.host = host
         self.port = port
 
-    def ping(self):
+    def ping(self, host=None, port=None):
+        host = host or self.host
+        port = port or self.port
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.host, self.port))
+            s.connect((host, port))
             s.shutdown(2)
             s.close()
         except Exception as e:
-            send_to_subscribers("Failed to ping " + self.host)
+            send_to_subscribers("Failed to ping " + host)
             return e
