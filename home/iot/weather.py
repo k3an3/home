067d77a7cc48e95dfc72e95133e26ca417c86a7a
Keane O'Kelley <keane.m.okelley@gmail.com>
2017-04-20 20:36:08 -0500

    Lots of quotes

--- home/iot/weather.py
+++ home/iot/weather.py
@@ -1,5 +1,6 @@
 import calendar
 import datetime
+import random
 
 import requests
 from flask import request, abort
@@ -10,6 +11,77 @@ from home.web.utils import api_auth_required
 from home.web.web import app
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/"
+
+RAINY_SAYINGS = (
+    "it's going to be a wet one.",
+    "I hope you saved something for a rainy day.",
+    "you might just get wet.",
+    "unless you want to get wet.",
+)
+
+WINDY_SAYINGS = (
+    "Hold on to your hat! It's going to be a breezy one.",
+    "Batten down the hatches! It may get windy.",
+    "Watch out for falling trees on this windy day!",
+    "The wind might just blow you over."
+)
+
+HOT_SAYINGS = (
+    "Drop your socks and put on some crocks! It's going to be a hot one.",
+    "Might want to stay inside, or go to the pool. It will be a bit hot.",
+    "Soak up that air conditioning! It's hot out.",
+    "Make sure to stay hydrated in this hot weather!"
+)
+
+COLD_SAYINGS = (
+    "It might be a hat and glove kind of day.",
+    "Try to keep warm!",
+    "The weather outside is frightful, but the inside is pretty nice.",
+    "Break out the handwarmers! It may be hard to type."
+)
+
+INSPIRATION = (
+    "Whatever you are, be a good one.",
+    "If you dream it, you can do it.",
+    "Every moment is a beginning",
+    "Never, never, never give up.",
+    "Don’t wait. The time will never be just right.",
+    "If not us, who? If not now, when?",
+    "Everything you can imagine is real.",
+    "I can, therefore I am.",
+    "Follow Your Bliss",
+    "Remember no one can make you feel inferior without your consent.",
+    "Turn your wounds into wisdom.",
+    "Wherever you go, go with all your heart.",
+    "Do what you can, with what you have, where you are.",
+    "Hope is a waking dream.",
+    "Action is the foundational key to all success.",
+    "Do one thing every day that scares you.",
+    "You must do the thing you think you cannot do.",
+    "Life is trying things to see if they work.",
+    "Don’t regret the past, just learn from it.",
+    "Believe you can and you’re halfway there.",
+    "Live what you love.",
+    "The power of imagination makes us infinite.",
+    "May you live every day of your life.",
+    "Eighty percent of success is showing up.",
+    "To be the best, you must be able to handle the worst.",
+    "A jug fills drop by drop.",
+    "The obstacle is the path.",
+    "The best revenge is massive success.",
+    "You get what you settle for",
+    "The best way out is always through.",
+    "If you have never failed you have never lived.",
+    "Hope is the heartbeat of the soul.",
+    "Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
+    "All you need is love.",
+    "It does not matter how slowly you go as long as you do not stop.",
+    "It is never too late to be what you might have been.",
+    "We become what we think about.",
+    "An obstacle is often a stepping stone.",
+    "Dream big and dare to fail.",
+    "Men are born to succeed, not fail.",
+)
 
 
 class Weather:
@@ -54,13 +126,14 @@ def format_weather(speech):
     dt = datetime.datetime.now()
     extra = []
     if weather.windy() or forecast.windy():
-        extra.append("Hold on to your hat! It's going to be a breezy one.")
+        extra.append(random.choice(WINDY_SAYINGS))
     if weather.rainy() or forecast.rainy():
-        extra.append("Don't forget to bring an umbrella!")
+        extra.append("Don't forget to bring an umbrella, " + random.choice(RAINY_SAYINGS))
     if weather.temp > 90 or forecast.temp > 90:
-        extra.append("It's gonna get toasty!")
+        extra.append(random.choice(HOT_SAYINGS))
     if weather.temp < 28 or forecast.temp < 28:
-        extra.append("It's a hat and glove kind of day.")
+        extra.append(random.choice(COLD_SAYINGS))
+    extra.append(random.choice(INSPIRATION))
     return message.format(part_of_day(dt), speech.name,
                           calendar.day_name[dt.weekday()],
                           calendar.month_name[dt.month],
