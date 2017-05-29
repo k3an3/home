ede9381797c075523f15d2583741a4174b04068d
Keane O'Kelley <kokelley@iastate.edu>
2017-05-29 12:56:45 -0400

    Can't get maps to appear

--- home/iot/tracker.py
+++ home/iot/tracker.py
@@ -5,13 +5,13 @@ from flask_socketio import join_room, emit
 
 from home.core.models import get_device
 from home.settings import DEBUG
-from home.web.utils import api_auth_required
+from home.web.utils import api_auth_required, ws_login_required
 from home.web.web import socketio, app
 
 
 class TrackedDevice:
-    def __init__(self, id_: str):
-        self.id = id_
+    def __init__(self, token: str):
+        self.id = token
         self.sid = None
 
     def cmd(self, cmd: str):
@@ -42,9 +42,31 @@ def ws_android_auth(f):
     return wrapped
 
 
+@socketio.on('exec')
+@ws_login_required
+def exec_cmd(data):
+    emit('cmd', {'cmd': data['cmd'], 'root': data['root'], 'noroot': data['noroot']},
+         namespace='/tracker', room="asdf")
+
+
+# Android to Server #
 @socketio.on('register', namespace='/tracker')
 @ws_android_auth
 def register(data):
     print(data['id'], "tried to register")
     join_room("asdf")
     emit('registered', 'registered')
+
+
+@socketio.on('result', namespace='/tracker')
+@ws_android_auth
+def result(data):
+    print('Got results', data)
+    emit('results', data, namespace="/", broadcast=True)
+
+
+@socketio.on('location', namespace='/tracker')
+@ws_android_auth
+def result(data):
+    print('Got location', data)
+    emit('location', data, namespace="/", broadcast=True)
---
+++ home/web/static/js/tracker.js
@@ -0,0 +1,32 @@
+/*
+function initMap() {
+    var usa = {lat: 39.828, lng: 98.579};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: usa
+    });
+}
+
+ */
+function updateLocation(lat, lon) {
+    map.setCenter({lat: lat, nlg: lon});
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: lon},
+        map: map
+    });
+}
+
+$('#exec').click(function() {
+    ws.emit('exec', {cmd: $('#cmd').val(),
+    root: $('#force-root').is(':checked'),
+    noroot: $('#force-noroot').is(':checked')});
+});
+
+ws.on('location', function(e) {
+    updateLocation(e.lat, e.lon);
+});
+
+ws.on('results', function(result) {
+    $('#results').html(result.data);
+    $('#results').show();
+});
