c1d46121d0e38523f37f65ee4b24863c11f26176
Keane O'Kelley <kokelley@iastate.edu>
2017-09-05 14:15:33 -0500

    Fix for chromecast

--- home/iot/chromecast.py
+++ home/iot/chromecast.py
@@ -4,10 +4,9 @@ chromecast.py
 
 Module to wrap pychromecast
 """
+from functools import wraps
 
 import pychromecast
-
-from home.core.utils import method_from_name
 
 
 def get_chromecast(host: str = None, name: str = None):
@@ -42,42 +41,55 @@ class Chromecast:
         self.name = name
         self.port = port
         self.cec = cec
-        self.cast = get_chromecast(host, name)
 
-    def command(self, command: str, **kwargs):
-        self.cast = get_chromecast(self.host, self.name)
-        method_from_name(self, command)(**kwargs)
+    def get_cast(f):
+        @wraps(f)
+        def wrapped(self, *args, **kwargs):
+            kwargs['cast'] = get_chromecast(self.host, self.name)
+            return f(self, *args, **kwargs)
 
-    def test_cast(self):
+        return wrapped
+
+    @get_cast
+    def test_cast(self, cast):
         self.cast.media_controller.play_media(
             'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
             'video/mp4')
         self.cast.media_controller.block_until_active(20)
 
-    def cast_media(self, url, type='video/mp4'):
-        self.cast.media_controller.play_media(url, type)
-        self.cast.media_controller.block_until_active(20)
+    @get_cast
+    def cast_media(self, cast, url, type='video/mp4'):
+        cast.media_controller.play_media(url, type)
+        cast.media_controller.block_until_active(20)
 
-    def get_status(self):
-        return self.cast.media_controller.status
+    @get_cast
+    def get_status(self, cast):
+        return cast.media_controller.status
 
-    def stop(self):
-        self.cast.media_controller.stop()
+    @get_cast
+    def stop(self, cast):
+        cast.media_controller.stop()
 
-    def pause(self):
-        self.cast.media_controller.pause()
+    @get_cast
+    def pause(self, cast):
+        cast.media_controller.pause()
 
-    def play(self):
-        self.cast.media_controller.play()
+    @get_cast
+    def play(self, cast):
+        cast.media_controller.play()
 
-    def reboot(self):
-        self.cast.reboot()
+    @get_cast
+    def reboot(self, cast):
+        cast.reboot()
 
-    def quit(self):
-        self.cast.quit_app()
+    @get_cast
+    def quit(self, cast):
+        cast.quit_app()
 
-    def volume_up(self, delta=0.1):
-        self.cast.volume_up(delta)
+    @get_cast
+    def volume_up(self, cast, delta=0.1):
+        cast.volume_up(delta)
 
-    def volume_down(self, delta=0.1):
-        self.cast.volume_down(delta)
+    @get_cast
+    def volume_down(self, cast, delta=0.1):
+        cast.volume_down(delta)
