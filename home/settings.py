f8e44bc32fc61ac8df4deb3865573bf13d0cacfb
Keane O'Kelley <kokelley@iastate.edu>
2018-04-16 11:47:47 -0500

    Fix some issues with missing settings and scheduler

--- home/settings.py
+++ home/settings.py
@@ -12,14 +12,20 @@ SECURITY_FOOTAGE_DIRS = ('/media/external/cam1', '/media/external/cam2')
 TEMPLATE_DIR = 'iot'
 PUBLIC_GROUPS = ('living_room', 'general')
 USE_LDAP = False
+LDAP_HOST = ''
 LDAP_PORT = 389
 LDAP_SSL = False
 LDAP_FILTER = "uid={}"
 LDAP_ADMIN_GID = 10000
+LDAP_BASE_DN = ''
 SPOTIFY_API_KEY = 'your_key_here'
 ASYNC_MODE = 'multiprocessing'
 MEDIA_TEST_URI = '/test.mp4'
 SENTRY_URL = ''
+SMTP_SERVER = ''
+SMTP_PORT = 25
+SMTP_USERNAME = ''
+SMTP_PASSWORD = ''
 db = SqliteDatabase('app.db')
 # db = MySQLDatabase(host="localhost", database="home", user="home", passwd="home")
 
